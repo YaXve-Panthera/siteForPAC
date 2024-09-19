e771cf0dd386c111a041ffde328508a851a84d82
kosmoNotebook <clplaycom@gmail.com>
2024-09-19 12:37:44 +0500

    htmls

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, template_rendered, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Hello World'
+    return render_template("home.html")
+
+@app.route('/registration')
+def registration():
+    return render_template("registration.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
