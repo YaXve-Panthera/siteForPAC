58da2f5af71ff90ee9e624953d0a269f6f5e5d9f
Ilya <clplaycom@gmail.com>
2024-10-08 00:06:47 +0500

    update code

--- user.py
+++ user.py
@@ -8,8 +8,8 @@ class User(UserMixin):
     db = DataBase("siteBase")
 
     def fromDB(self, user_id):
-        print("request to DB")
-        print(user_id)
+        # print("request to DB")
+        # print(user_id)
         if user_id == "":
             return False
         self.__user = self.db.get_user(user_id)
